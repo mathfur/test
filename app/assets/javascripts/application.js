7c32b912b36aad4ed1bfb4efbefdd73e6e142eba
mathfuru <mathfuru@gmail.com>
2012-12-02 19:27:13 +0900

    Add jQuery UI Layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,40 @@
 //= require jquery_ujs
 //= require jquery.masonry
 //= require jquery.infinitescroll
+//= require jquery.layout
+
+$(function(){
+  $('body').layout({
+    applyDefaultStyles: true,
+    north__closable: false,
+    east__closable: true,
+    south__closable: false,
+    west__closable: true,
+    north__resizable: false,
+    east__resizable: true,
+    south__resizable: false,
+    west__resizable: true,
+
+    slideTrigger_close: 'mouseover',
+    east__togglerTip_open: "開く",
+
+    togglerAlign_open: "top",
+    togglerAlign_closed: "top",
+
+    togglerLength_open: 30, // トグル領域の長さ
+    togglerLength_closed: 30, // トグル領域の長さ
+
+    sliderCursor: 'pointer',
+
+    fxName: 'scale',
+    fxSpeed: 'slow',
+
+    east__initClosed: true,
+
+    east__togglerContent_open: "<span>OPEN</span>",
+    east__spacing_open: 30,
+    east__spacing_closed: 30,
+
+    west__size: 200,
+  });
+})
